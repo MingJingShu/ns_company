9c2d26936f18b3b9ad073b43068f823a05214e59
MingJing <a0911077842@gmail.com>
2020-09-08 10:28:50 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+console.log("hello");
+
+
+$('.content').slick({
+
+    slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,  
+   dots: true,
+   focusOnSelect: true
+  });
+    
+
+
+
+});
