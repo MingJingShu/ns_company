e000d75d8554f99f0c89355f4fb004edfab3e145
MingJing <a0911077842@gmail.com>
2020-09-13 16:41:59 +0800

    hamburger basic

--- js/all.js
+++ js/all.js
@@ -1,17 +1,24 @@
-$(document).ready(function(){
-console.log("hello");
+$(document).ready(function () {
+  console.log("hello");
 
-
-$('.slick_w').slick({
-
+  $(".slick_w").slick({
     slidesToShow: 1,
-   slidesToScroll: 1,
-   autoplay: true,  
-   dots: true,
-   focusOnSelect: true
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    focusOnSelect: true,
   });
-    
 
-
-
+  /*hamburger*/
+  let hamburger = $("footer .hamburger");
+  let h_w = $(".hamburger_window");
+  hamburger.click(function () {
+    if (h_w.hasClass("active") != true) {
+      h_w.addClass('active');
+      hamburger.addClass('active');
+    }else{
+      h_w.removeClass('active');
+      hamburger.removeClass('active');
+    }
+  });
 });
